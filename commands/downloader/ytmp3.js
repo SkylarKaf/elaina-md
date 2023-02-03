4e571571d13189f1b958e5a20b5d95a3c00f7f17
Andik Fr <91518502+SkylarKaf@users.noreply.github.com>
2023-02-03 08:52:22 +0700

    Update ytmp3.js

--- commands/downloader/ytmp3.js
+++ commands/downloader/ytmp3.js
@@ -1,18 +1,42 @@
-let { yta } = require("../../lib/y2mate");
 module.exports = {
 	name: "ytmp3",
 	param: "<url>",
-	cmd: ["ytmp3", "ytaudio", "yta"],
+	cmd: ["ytmp3", "yta", "ytaudio"],
 	category: "downloader",
 	desc: "download audio from youtube",
 	query: true,
 	url: true,
 	async handler(m, { conn, text }) {
-		conn.sendReact(m.from, '🕒', m.key)	
-          let quality = '128kbps'
-          let media = await yta(text, quality)	         
-          let thumbs = {url: media.thumb}		
-          if (media.filesize >= 999999) return m.reply('*AKSES DITOLAK!*, size Media terlalu besar! '+util.format(media))
-	    conn.sendMessage(m.from, { document: {url:media.dl_link}, fileName: `${media.title}.mp3`, mimetype: 'audio/mp3', jpegThumbnail: await tool.resize(thumbs, 300, 200)}, { quoted: m })						
-    }
-}
+		conn.sendReact(m.from, '🕒', m.key)
+		const down = await scrapp.youtube("mp3", await scrapp.expandUrl(text));
+		const link = down.link;
+		down.author = down.author.name;
+		const mdata = {
+			title: down.title,
+			album: config.botname,
+			artist: await conn.getName(m.sender),
+			image: {
+				mime: 'image/png',
+				type: {
+					id: 3,
+					name: 'front cover'
+				},
+				imageBuffer: await tool.getBuffer(down.thumbnail)
+			}
+		}
+		if (link == undefined) return m.reply("Cannot find download url!");
+		if (m.type != "templateButtonReplyMessage" && m.type != "buttonsResponseMessage")
+		try{
+			const tsize = down.size.split(' ')[1]
+			if(down.size.split('.')[0].split(' ')[0] > 100 && tsize != 'KB' || tsize == "GB") return m.reply(`Oversize, silahkan download melalui link dibawah\n${await tool.tiny(link)}`)
+			await conn.sendMessage(m.from,{ document: { url: link }, mimetype: "audio/mpeg", fileName: mdata.title},{ quoted: m });
+		}catch{
+			const down = await scrapp.y1s('mp3', await scrapp.expandUrl(text))
+			if(!down.status) return m.reply(down)
+			if(!down.dlink) return m.reply("Cannot find download url!");
+			const tsize = down.size.split(' ')[1]
+			if(down.size.split('.')[0].split(' ')[0] > 100 && tsize != 'KB' || tsize == "GB") return m.reply(`Oversize, silahkan download melalui link dibawah\n${await tool.tiny(down.dlink)}`)
+			await conn.sendMessage(m.from, {document: {url: down.dlink}, mimetype: 'audio/mpeg', fileName: mdata.title}, {quoted: m})
+		}
+    },
+};
