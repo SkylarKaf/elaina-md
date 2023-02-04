301bb69b3e5964894245dfc22ce767be53e809ba
Andik Fr <91518502+SkylarKaf@users.noreply.github.com>
2023-02-04 20:00:45 +0700

    Update ytmp4.js
    Banyak yg bilang error ternyata :/

--- commands/downloader/ytmp4.js
+++ commands/downloader/ytmp4.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: "ytmp4",
 	param: "<url>",
-	cmd: ["ytmp4", "ytv", "ytvideo"],
+	cmd: ["ytmp4", "yta", "ytvideo"],
 	category: "downloader",
 	desc: "download video from youtube",
 	query: true,
@@ -16,14 +16,14 @@ module.exports = {
 	   try{
 			const tsize = down.size.split(' ')[1]
 			if(down.size.split('.')[0].split(' ')[0] > 150 && tsize != 'KB' || tsize == "GB") return m.reply(`Oversize, silahkan download melalui link dibawah\n${await tool.tiny(link)}`)
-			await conn.sendMessage(m.from, { document: { url: link }, mimetype: "video/mp4", fileName: down.title }, { quoted: m});
+			await conn.sendMessage(m.from, { video: { url: link }, mimetype: "video/mp4", fileName: down.title }, { quoted: m});
 		}catch{
 			const down = await scrapp.y1s('mp4', await scrapp.expandUrl(text))
 			if(!down.status) return m.reply(down)
 			if(!down.dlink) return m.reply("Cannot find download url!");
 			const tsize = down.size.split(' ')[1]
 			if(down.size.split('.')[0].split(' ')[0] > 150 && tsize != 'KB' || tsize == "GB") return m.reply(`Oversize, silahkan download melalui link dibawah\n${await tool.tiny(down.dlink)}`)
-			await conn.sendMessage(m.from, { document: { url: down.dlink }, mimetype: "video/mp4", fileName: down.title }, {quoted: m})
+			await conn.sendMessage(m.from, { video: { url: down.dlink }, mimetype: "video/mp4", fileName: down.title }, {quoted: m})
 		}
 	},
 };
