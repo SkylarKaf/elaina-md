ec90511538e90901eead0214ad4c81bb3a9bd16c
Andik Fr <91518502+SkylarKaf@users.noreply.github.com>
2023-02-06 08:44:11 +0700

    Ganti ke list aja

--- commands/downloader/play.js
+++ commands/downloader/play.js
@@ -1,45 +1,30 @@
-const yts = require("yt-search");
+const yts = require('yt-search')
 module.exports = {
-	name: "play",
-	param: "<query>",
-	cmd: ["play"],
-	category: "downloader",
-	desc: "Search songs from youtube and download it",
-	query: true,
-	async handler(m, { conn, text }) {
-		conn.sendReact(m.from, '🕒', m.key)
-          const search = await yts(text)
-          let anu = search.videos[Math.floor(Math.random() * search.videos.length)]
-           var buttons = [
-           {buttonId: `.ytmp3 ${anu.url}`, buttonText: {displayText: 'Audio'}, type: 1},
-           {buttonId: `.ytmp4 ${anu.url}`, buttonText: {displayText: 'Video'}, type: 1}
-            ]
-          var txt = `*•Title:* ${anu.title}\n\n`
-          txt += `*•Duration:* ${anu.timestamp}\n`
-          txt += `*•Viewers:* ${anu.views}\n`
-          txt += `*•Uploaded:* ${anu.ago}\n`
-          txt += `*•Author:* ${anu.author.name}\n`
-          txt += `*•Description:* ${anu.description}`
-          let ytgmr = await tool.getBuffer(anu.thumbnail)
-          let buttonMessage = {
-          document : ytgmr,
-          fileName: `${anu.title}`,
-          fileLength: `${anu.timestamp}`,
-          pageCount: `${anu.views}`,
-          caption: txt,
-          footer: ``,
-          buttons: buttons,
-          headerType: 4,
-          contextInfo: { externalAdReply: {
-          title: `⇆ㅤ    ㅤ  ◁ㅤ   ❚❚   ㅤ▷ ㅤ       ㅤ↻`,
-          body: ``,
-          thumbnail: ytgmr,
-          mediaType: 2,
-          showAdAttribution: true,
-          mediaUrl: anu.url,
-          sourceUrl: anu.url
-          }}
-         }
-        conn.sendMessage(m.from, buttonMessage, { quoted: m })
-      }
-};
+  name: 'play',
+  param: '<query>',
+  cmd: ['ytsearch', 'yts', 'play'],
+  category: 'downloader',
+  desc: 'search video in web youtube.com',
+  query: true,
+  async handler(m, {conn, text}){
+    conn.sendReact(m.from, '🕒', m.key);
+    const url = await yts(text)
+    const filter = url.all.filter(s => s.type == 'video')
+    const listSerch = []
+    for (let i of filter) {
+      listSerch.push({
+        title: i.title, rows:[
+          {title: "Audio", rowId: `.ytmp3 ${i.url}`, description: `• Description : ${i.description}\n\n• Duration : ${i.timestamp}`},
+          {title: "Video", rowId: `.ytmp4 ${i.url}`, description: `• Description : ${i.description}\n\n• Duration : ${i.timestamp}`}]
+      })
+    }
+    var listMessage = {
+      text: `Result From ${text}`,
+      footer: config.botname,
+      title: "[ YouTube Search ]",
+      buttonText: "Click Here",
+      sections: listSerch
+    }
+    await conn.sendMessage(m.from, listMessage, {quoted: m})
+  }
+}
