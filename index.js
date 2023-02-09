fad17a9ab49b58c2802c0cd1e1dd4c6756038755
Andik Fr <91518502+SkylarKaf@users.noreply.github.com>
2023-02-09 09:40:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,57 @@
-console.log("Starting...");
-let { spawn } = require("child_process");
-let path = require("path");
-let fs = require("fs");
-let package = require("./package.json");
+console.log('Starting...')
+let cluster = require('cluster')
+let path = require('path')
+let fs = require('fs')
+let package = require('./package.json')
+const Readline = require('readline')
+const yargs = require('yargs/yargs')
+const rl = Readline.createInterface(process.stdin, process.stdout)
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
-var isRunning = false;
+var isRunning = false
 /**
  * Start a js file
  * @param {String} file `path/to/file`
  */
 function start(file) {
-	if (isRunning) return;
-	isRunning = true;
-	let args = [path.join(__dirname, file), ...process.argv.slice(2)];
-	let p = spawn(process.argv[0], args, {
-		stdio: ["inherit", "inherit", "inherit", "ipc"],
-	});
-	p.on("message", (data) => {
-		console.log("[RECEIVED]", data);
-		switch (data) {
-			case "reset":
-				p.kill();
-				isRunning = false;
-				start.apply(this, arguments);
-				break;
-			case "uptime":
-				p.send(process.uptime());
-				break;
-		}
-	});
-	p.on("exit", (code) => {
-		isRunning = false;
-		console.error("Exited with code:", code);
-		if (code === 0) return;
-		start('main.js')
-		fs.watchFile(args[0], () => {
-			fs.unwatchFile(args[0]);
-			start(file);
-		});
-	});
-	// console.log(p)
+  if (isRunning) return
+  isRunning = true
+  let args = [path.join(__dirname, file), ...process.argv.slice(2)]
+  console.log([process.argv[0], ...args].join(' '))
+  cluster.setupMaster({
+    exec: path.join(__dirname, file),
+    args: args.slice(1),
+  })
+  let p = cluster.fork()
+  p.on('message', data => {
+    console.log('[RECEIVED]', data)
+    switch (data) {
+      case 'reset':
+        p.kill()
+        isRunning = false
+        start.apply(this, arguments)
+        break
+      case 'uptime':
+        p.send(process.uptime())
+        break
+    }
+  })
+  p.on('exit', code => {
+    isRunning = false
+    console.error('Exited with code:', code)
+    if (code === 0) return
+    start('main.js')
+    fs.watchFile(args[0], () => {
+      fs.unwatchFile(args[0])
+      //start('main.js') start(file)
+    })
+  })
+  let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+  if (!opts['test'])
+    if (!rl.listenerCount()) rl.on('line', line => {
+      p.emit('message', line.trim())
+    })
+  // console.log(p)
 }
 
-start("main.js");
+start('main.js')
